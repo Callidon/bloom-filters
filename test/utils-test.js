4386cd31f9aaa61f15fe5ade175db6b926326a17
Thomas Minier <thomas.minier@protonmail.com>
2017-01-23 09:36:03 +0100

    bootstrap project

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,6 +1,7 @@
+/* file : utils-test.js
 MIT License
 
-Copyright (c) 2017 Thomas Minier & Arnaud Grall
+Copyright (c) 2016 Thomas Minier & Arnaud Grall
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,3 +20,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+'use strict';
