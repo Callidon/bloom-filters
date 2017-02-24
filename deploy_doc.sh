6f6cf5f090b7075b3c858fbeb5204e1d1c538488
Thomas Minier <thomas.minier@protonmail.com>
2017-02-24 13:27:12 +0100

    add auto deploy of gh-pages

---
+++ deploy_doc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# deploy jsdoc to gh-pages
+rev=$(git rev-parse --short HEAD)
+
+cd doc
+
+git init
+git config user.name "Thomas Minier"
+git config user.email "thomas.minier@protonmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/Callidon/bloom-filters.git"
+git fetch upstream && git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
