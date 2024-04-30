c4529d7cd56c20007d542db1537b7638f9ccbe4b
Arnaud Grall <folkvir@outlook.fr>
2024-04-30 21:11:54 +0200

    utis.test.ts eslint + jest ok

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -22,91 +22,115 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-require('chai').should()
-const {
+import { expect, describe, test } from '@jest/globals'
+import {
     allocateArray,
     randomInt,
     xorBuffer,
     getDefaultSeed,
     isEmptyBuffer,
-} = require('../dist/utils.js')
-const {BloomFilter, BaseFilter} = require('../dist/api.js')
-const XXH = require('@node-rs/xxhash')
-const {range} = require('lodash')
+} from '../src/utils'
+import { BloomFilter, BaseFilter } from '../src/api'
+import XXH from '@node-rs/xxhash'
+import { range } from 'lodash'
 const seed = getDefaultSeed()
-const {Hashing} = require('../dist/api.js')
+import { Hashing } from '../src/api'
 
 describe('Utils', () => {
     describe('#allocateArray', () => {
-        it('should allocate an array with the given size and a default value', () => {
+        test('should allocate an array with the given size and a default value', () => {
             const array = allocateArray(15, 1)
-            array.length.should.equal(15)
-            array.forEach(value => value.should.equal(1))
+            expect(array.length).toEqual(15)
+            array.forEach(value => {
+                expect(value).toEqual(1)
+            })
         })
 
-        it('should allow the use of a function to set the default value', () => {
+        test('should allow the use of a function to set the default value', () => {
             const array = allocateArray(15, () => 'foo')
-            array.length.should.equal(15)
-            array.forEach(value => value.should.equal('foo'))
+            expect(array.length).toEqual(15)
+            array.forEach(value => {
+                expect(value).toEqual('foo')
+            })
         })
     })
 
     describe('#doubleHashing', () => {
-        it('should perform a double hashing', () => {
+        test('should perform a double hashing', () => {
             const hashing = new Hashing()
-            const hashA = Math.random(Number.MIN_VALUE, Number.MAX_VALUE / 2)
-            const hashB = Math.random(Number.MAX_VALUE / 2, Number.MAX_VALUE)
+            function getRandomInt(min: number, max: number) {
+                // The maximum is exclusive and the minimum is inclusive
+                const minCeiled = Math.ceil(min)
+                const maxFloored = Math.floor(max)
+                return Math.floor(
+                    Math.random() * (maxFloored - minCeiled) + minCeiled
+                )
+            }
+            const hashA = getRandomInt(Number.MIN_VALUE, Number.MAX_VALUE / 2)
+            const hashB = getRandomInt(Number.MAX_VALUE / 2, Number.MAX_VALUE)
             const size = 1000
             const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
             values.forEach(n => {
-                hashing
-                    .doubleHashing(n, hashA, hashB, size)
-                    .should.equal((hashA + n * hashB + (n ** 3 - n) / 6) % size)
+                expect(hashing.doubleHashing(n, hashA, hashB, size)).toEqual(
+                    (hashA + n * hashB + (n ** 3 - n) / 6) % size
+                )
             })
         })
     })
 
     describe('#randomInt', () => {
-        it('should generate a random int in an interval', () => {
+        test('should generate a random int in an interval', () => {
             const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-            randomInt(values[0], values[9]).should.be.oneOf(values)
+            expect(values).toContain(randomInt(values[0], values[9]))
         })
     })
 
     describe('#xorBuffer', () => {
-        it('should xor correctly 2 Buffers', () => {
+        test('should xor correctly 2 Buffers', () => {
             const a = Buffer.allocUnsafe(10).fill(0)
             const b = Buffer.alloc(1, 1)
             const res = Buffer.allocUnsafe(10).fill(0)
             res[res.length - 1] = 1
             // xor(a, b) = <Buffer 00 00 00 00 00 00 00 00 00 01>
-            xorBuffer(Buffer.from(a), Buffer.from(b))
-                .toString()
-                .should.equal(b.toString())
+            expect(
+                xorBuffer(Buffer.from(a), Buffer.from(b)).toString()
+            ).toEqual(b.toString())
             // xor(xor(a, b), b) === a <Buffer 00 00 00 00 00 00 00 00 00 00> === <Buffer />
-            xorBuffer(xorBuffer(Buffer.from(a), Buffer.from(b)), Buffer.from(b))
-                .toString()
-                .should.equal(Buffer.from('').toString())
+            expect(
+                xorBuffer(
+                    xorBuffer(Buffer.from(a), Buffer.from(b)),
+                    Buffer.from(b)
+                ).toString()
+            ).toEqual(Buffer.from('').toString())
             // xor(xor(a, b), a) === b
-            xorBuffer(xorBuffer(Buffer.from(a), Buffer.from(b)), Buffer.from(a))
-                .toString()
-                .should.equal(Buffer.from(b).toString())
+            expect(
+                xorBuffer(
+                    xorBuffer(Buffer.from(a), Buffer.from(b)),
+                    Buffer.from(a)
+                ).toString()
+            ).toEqual(Buffer.from(b).toString())
             // xor(xor(a, a), a) === a
-            xorBuffer(xorBuffer(Buffer.from(a), Buffer.from(a)), Buffer.from(a))
-                .toString()
-                .should.equal(Buffer.from('').toString())
+            expect(
+                xorBuffer(
+                    xorBuffer(Buffer.from(a), Buffer.from(a)),
+                    Buffer.from(a)
+                ).toString()
+            ).toEqual(Buffer.from('').toString())
             // xor(xor(b, b), b) === a
-            xorBuffer(xorBuffer(Buffer.from(b), Buffer.from(b)), Buffer.from(b))
-                .toString()
-                .should.equal(Buffer.from(b).toString())
+            expect(
+                xorBuffer(
+                    xorBuffer(Buffer.from(b), Buffer.from(b)),
+                    Buffer.from(b)
+                ).toString()
+            ).toEqual(Buffer.from(b).toString())
         })
-        it('should xor correctly', () => {
+        test('should xor correctly', () => {
             let a = Buffer.allocUnsafe(1).fill(1)
             let b = Buffer.allocUnsafe(1).fill(1)
             const max = 100
             let last
             for (let i = 0; i < max; i++) {
-                const s = XXH.xxh32('' + i, seed).toString(16)
+                const s = XXH.xxh32(i.toString(), seed).toString(16)
                 const buf = Buffer.from(s)
                 a = xorBuffer(a, buf)
                 if (i !== max - 1) {
@@ -115,20 +139,20 @@ describe('Utils', () => {
                     last = buf
                 }
             }
-            xorBuffer(a, b).equals(last).should.equal(true)
-            xorBuffer(a, b).toString().should.equal(last.toString())
-            xorBuffer(a, a).equals(Buffer.allocUnsafe(0)).should.equal(true)
-            xorBuffer(b, b).equals(Buffer.allocUnsafe(0)).should.equal(true)
+            expect(xorBuffer(a, b).equals(last)).toBe(true)
+            expect(xorBuffer(a, b).toString()).toEqual(last.toString())
+            expect(xorBuffer(a, a).equals(Buffer.allocUnsafe(0))).toBe(true)
+            expect(xorBuffer(b, b).equals(Buffer.allocUnsafe(0))).toBe(true)
         })
     })
 
     describe('#isBufferEmpty', () => {
-        it('should return true if a buffer is empty', () => {
-            isEmptyBuffer(Buffer.allocUnsafe(10).fill(0)).should.equal(true)
-            isEmptyBuffer(Buffer.allocUnsafe(0).fill(0)).should.equal(true)
+        test('should return true if a buffer is empty', () => {
+            expect(isEmptyBuffer(Buffer.allocUnsafe(10).fill(0))).toBe(true)
+            expect(isEmptyBuffer(Buffer.allocUnsafe(0).fill(0))).toBe(true)
         })
-        it('should return false if a buffer is not empty', () => {
-            isEmptyBuffer(Buffer.allocUnsafe(10).fill(1)).should.equal(false)
+        test('should return false if a buffer is not empty', () => {
+            expect(isEmptyBuffer(Buffer.allocUnsafe(10).fill(1))).toBe(false)
         })
     })
 
@@ -137,41 +161,38 @@ describe('Utils', () => {
             'da5e21f8a67c4163f1a53ef43515bd027967da305ecfc741b2c3f40f832b7f82'
         const desiredIndices = 10000
         const result = range(0, desiredIndices, 1)
-        it(`should return ${desiredIndices} distinct indices on the interval [0, ${desiredIndices})`, () => {
+        test(`should return ${desiredIndices.toString()} distinct indices on the interval [0, ${desiredIndices.toString()})`, () => {
             try {
                 const obj = new (class extends BaseFilter {})()
                 const start = new Date().getTime()
                 const indices = obj._hashing
                     .getDistinctIndexes(key, desiredIndices, desiredIndices)
                     .sort((a, b) => a - b)
-                indices.should.deep.equal(result)
+                expect(indices).toEqual(result)
                 console.log(
-                    `Generated ${
-                        indices.length
-                    } distinct indices on the interval [0, ${desiredIndices}) in ${
+                    `Generated ${indices.length.toString()} distinct indices on the interval [0, ${desiredIndices.toString()}) in ${(
                         new Date().getTime() - start
-                    } ms`
+                    ).toString()} ms`
                 )
             } catch (e) {
-                throw Error('it should not throw: ' + e)
+                throw Error(e)
             }
         })
-        it('should the issue be fixed', () => {
+        test('should the issue be fixed', () => {
             try {
                 const filter = new BloomFilter(39, 28)
                 filter.add(key)
-                filter.has(key).should.be.true
+                expect(filter.has(key)).toBe(true)
             } catch (e) {
-                throw Error('it should not throw: ' + e)
+                throw Error(e)
             }
         })
     })
 
     describe('Use different hash functions', () => {
-        it('overriding serialize function by always returning Number(1)', () => {
+        test('overriding serialize function by always returning Number(1)', () => {
             class CustomHashing extends Hashing {
-                serialize(_element, _seed = undefined) {
-                    // eslint-disable-line
+                serialize() {
                     return Number(1)
                 }
             }
@@ -182,7 +203,7 @@ describe('Utils', () => {
             bl2._hashing = new CustomHashing()
             bl2.add('b')
             // 2 bloom filters with a hash functions returning everytime the same thing must be equal
-            bl.equals(bl2).should.be.true
+            expect(bl.equals(bl2)).toBe(true)
         })
     })
 })
